bfeb5cced79745ea4466e6b59fdfcc4463659707
Yuwetha Nesan <67994072+YuwethaNesan@users.noreply.github.com>
2024-05-22 00:47:35 +0530

    Update emotionDetection.py

--- emotion-detection/app/emotionDetection.py
+++ emotion-detection/app/emotionDetection.py
@@ -1,23 +1,23 @@
-from fer import FER
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-import tensorflow as tf
+# from fer import FER
+# import matplotlib.pyplot as plt
+# import matplotlib.patches as patches
+# import tensorflow as tf
 
-def getEmotion(file):
-    detector = FER(mtcnn=True)
-    img = plt.imread(file)
-    result = detector.detect_emotions(img)
-    if result:
-        emotion, score = _getTopEmotion(result)
-        return [emotion, score]
-    else:
-        return []
+# def getEmotion(file):
+#     detector = FER(mtcnn=True)
+#     img = plt.imread(file)
+#     result = detector.detect_emotions(img)
+#     if result:
+#         emotion, score = _getTopEmotion(result)
+#         return [emotion, score]
+#     else:
+#         return []
 
 
-# Load your trained model
-# model_path = "./model_weights_FER.weights.h5"
-# model = tf.keras.models.load_model(model_path)
+# # Load your trained model
+# # model_path = "./model_weights_FER.weights.h5"
+# # model = tf.keras.models.load_model(model_path)
 
-def _getTopEmotion(result):
-    return sorted(list(result[0]["emotions"].items()), key=lambda x: x[1])[-1]
+# def _getTopEmotion(result):
+#     return sorted(list(result[0]["emotions"].items()), key=lambda x: x[1])[-1]
 
